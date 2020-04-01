8bed9531f42dff5b7a7625ceb2f7c2b956ac6d9d
Mahmoud-joo <56273659+Mahmoud-joo@users.noreply.github.com>
2020-04-01 09:44:01 -0500

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,1 +1,5 @@
-pip install subprocess.run
+import subprocess 
+subprocess.run('apt update')
+subprocess.run('apt install python3.8')
+subprocess.run('apt install tshark')
+subprocess.run('apt install nmap')
