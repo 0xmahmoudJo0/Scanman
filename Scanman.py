09424226817e034996f45deaf086708495c62e38
Mahmoud-joo <56273659+Mahmoud-joo@users.noreply.github.com>
2020-04-02 15:35:50 -0500

    Update Scanman.py

--- Scanman.py
+++ Scanman.py
@@ -12,10 +12,10 @@ print ("BY:MahmoudJOO")
 import os,sys
 import subprocess
 
+
 a = input ("Enter The Pcap file:  ")
 
-
-print (" $$$$$$$$$$$$$$$$$$   PLEASE WAIT UNTIL SCAN FINISH :)    $$$$$$$$$$$$$$$$$$$  ")
+ $$$$$$$$$$$$$$$$$$   PLEASE WAIT UNTIL SCAN FINISH :)    $$$$$$$$$$$$$$$$$$$  
 
 subprocess.run(f'tshark -T fields -e ip.src -r {a}>>SM1.txt ', shell=True)
 subprocess.run('cat -n SM1.txt | sort -uk2 | sort -nk1 | cut -f2->>HOSTip.txt', shell=True)
