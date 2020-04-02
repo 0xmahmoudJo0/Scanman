0461d53bf06a7d0ab4d9c277048759302e45df10
Mahmoud-joo <56273659+Mahmoud-joo@users.noreply.github.com>
2020-04-02 07:27:45 -0500

    Update Scanman.py

--- Scanman.py
+++ Scanman.py
@@ -21,7 +21,7 @@ print (" $$$$$$$$$$$$$$$$$$   PLEASE WAIT UNTIL SCAN FINISH :)    $$$$$$$$$$$$$$
 
 
 subprocess.run(f'tshark -T fields -e ip.src -r {a}>>SM1.txt ', shell=True)
-subprocess.run(f'cat -n SM1.txt | sort -uk2 | sort -nk1 | cut -f2->>HOSTip.txt', shell=True)
+subprocess.run('cat -n SM1.txt | sort -uk2 | sort -nk1 | cut -f2->>HOSTip.txt', shell=True)
 subprocess.run('nmap -T4  -O -iL HOSTip.txt >> HOSTos.txt', shell=True)
 subprocess.run('nmap -T4  -p- --open  -iL HOSTip.txt >> HOSTopenports.txt', shell=True)
 subprocess.run('nmap -T4  -sO -iL HOSTip.txt >> HOSTidenproto.txt', shell=True)
