f2fefa1c041a9e6495280fc41ef12bb2037f2fa8
Mahmoud-joo <56273659+Mahmoud-joo@users.noreply.github.com>
2020-04-04 11:52:13 -0500

    Update Scanman.py

--- Scanman.py
+++ Scanman.py
@@ -13,12 +13,13 @@ import os,sys
 import subprocess
 
 
-a = input ("Enter The Pcap file:  ")
+a = input ("Enter The Pcap File Path:  ")
 
 print ("$$$$$$$$$$$$$$$$$$   PLEASE WAIT UNTIL SCAN FINISH :)    $$$$$$$$$$$$$$$$$$$")  
 
-subprocess.run(f'tshark -T fields -e ip.src -r {a}>>SM1.txt ', shell=True)
-subprocess.run('cat -n SM1.txt | sort -uk2 | sort -nk1 | cut -f2->>HOSTip.txt', shell=True)
+subprocess.run(f'tshark -T fields -e ip.src -r {a}>SM1.txt ', shell=True)
+subprocess.run('cat -n SM1.txt | sort -uk2 | sort -nk1 | cut -f2->HOSTip.txt', shell=True)
 subprocess.run('nmap  -T5 -sO -iL HOSTip.txt -oG -> RESULTS.txt && nmap -T5 -O -iL HOSTip.txt >> RESULTS.txt' , shell=True )
 print ("the ip hosts in HOSTip.txt   ")
 print ("the output is in RESULTS.txt ")
+
