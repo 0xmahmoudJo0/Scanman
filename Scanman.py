0350e808104d1958caec0de04861019d3d2027a6
Mahmoud-joo <56273659+Mahmoud-joo@users.noreply.github.com>
2020-04-08 14:43:32 -0500

    Update Scanman.py

--- Scanman.py
+++ Scanman.py
@@ -13,13 +13,20 @@ import os,sys
 import subprocess
 
 
-a = input ("Enter The Pcap File Path:  ")
+a = input ("Enter The Pcap File Path:  " '\n')
 
-print ("$$$$$$$$$$$$$$$$$$   PLEASE WAIT UNTIL SCAN FINISH :)    $$$$$$$$$$$$$$$$$$$")  
+print ("$$$$$$$$$$$$$$$$$$   PLEASE WAIT UNTIL SCAN FINISH :)    $$$$$$$$$$$$$$$$$$$" '\n')  
 
 subprocess.run(f'tshark -T fields -e ip.src -r {a}>SM1.txt ', shell=True ,text=False)
 subprocess.run('cat -n SM1.txt | sort -uk2 | sort -nk1 | cut -f2->HOSTip.txt', shell=True)
-subprocess.run('nmap -T5  -p- -iL HOSTip.txt -oG - | grep "open"  > RESULTS.txt && nmap  -T5 -sO -iL HOSTip.txt -oG - | grep -v "Nmap\|Status" >> RESULTS.txt && nmap -T5  -O -iL HOSTip.txt | grep "Aggressive OS guesses" | grep -v "gnored" >> RESULTS.txt' , shell=True)
+
+subprocess.run('nmap  -T5 -sO -iL HOSTip.txt -oG - | grep -Ev "Nmap|Status" > RESULTS.txt ' , shell=True )
+print ("SCAN Identified protocol DONE :    " '\n')
+
+subprocess.run('echo -------------------------------------------------------------------------------------------------------- >> RESULTS.txt ' , shell=True)
+
+subprocess.run('nmap -O -T5 --open -iL HOSTip.txt | grep -Ev "shown|Warning|detection|hops|done|Starting|Host|closed" >> RESULTS.txt ' , shell=True)
+print ("SCAN OS & Open Ports DONE :        " '\n')
 
 print ("the ip hosts in HOSTip.txt   ")
 print ("the output is in RESULTS.txt ")
