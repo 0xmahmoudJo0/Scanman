3f12b41cc5e87e968ab1530c3281c79e9283387f
Mahmoud-joo <56273659+Mahmoud-joo@users.noreply.github.com>
2020-04-09 18:19:56 -0500

    Update Scanman.py

--- Scanman.py
+++ Scanman.py
@@ -1,50 +1,60 @@
 #!/usr/bin/env python3
-print ("  /      \                               /  \     /  |                      ")
-print (" /$$$$$$  |  _______   ______   _______  $$  \   /$$ |  ______   _______    ")
-print (" $$ \__$$/  /       | /      \ /       \ $$$  \ /$$$ | /      \ /       \   ")
-print (" $$      \ /$$$$$$$/  $$$$$$  |$$$$$$$  |$$$$  /$$$$ | $$$$$$  |$$$$$$$  |  ")
-print ("  $$$$$$  |$$ |       /    $$ |$$ |  $$ |$$ $$ $$/$$ | /    $$ |$$ |  $$ |  ")
-print (" /  \__$$ |$$ \_____ /$$$$$$$ |$$ |  $$ |$$ |$$$/ $$ |/$$$$$$$ |$$ |  $$ |  ")
-print (" $$    $$/ $$       |$$    $$ |$$ |  $$ |$$ | $/  $$ |$$    $$ |$$ |  $$ |  ")
-print ("  $$$$$$/   $$$$$$$/  $$$$$$$/ $$/   $$/ $$/      $$/  $$$$$$$/ $$/   $$/   ")
-print ("BY:MahmoudJOO")
 
 import os,sys
 import subprocess
+from colorama import Fore
+from colorama import Style
 
+print(f'{Fore.RED}    ______                                                                  {Style.RESET_ALL}')
+print(f'{Fore.RED}   /      \                                                                 {Style.RESET_ALL}')
+print(f'{Fore.RED}  /$$$$$$  |  _______   ______   _______   _____  ____    ______   _______  {Style.RESET_ALL}')
+print(f'{Fore.RED}  $$ \__$$/  /       | /      \ /       \ /     \/    \  /      \ /       \ {Style.RESET_ALL}')
+print(f'{Fore.RED}  $$      \ /$$$$$$$/  $$$$$$  |$$$$$$$  |$$$$$$ $$$$  | $$$$$$  |$$$$$$$  |{Style.RESET_ALL}')
+print(f'{Fore.WHITE}   $$$$$$  |$$ |       /    $$ |$$ |  $$ |$$ | $$ | $$ | /    $$ |$$ |  $$ |{Style.RESET_ALL}')
+print(f'{Fore.WHITE}  /  \__$$ |$$ \_____ /$$$$$$$ |$$ |  $$ |$$ | $$ | $$ |/$$$$$$$ |$$ |  $$ |{Style.RESET_ALL}')
+print(f'{Fore.RED}  $$    $$/ $$       |$$    $$ |$$ |  $$ |$$ | $$ | $$ |$$    $$ |$$ |  $$ |{Style.RESET_ALL}')
+print(f'{Fore.RED}   $$$$$$/   $$$$$$$/  $$$$$$$/ $$/   $$/ $$/  $$/  $$/  $$$$$$$/ $$/   $$/ {Style.RESET_ALL}')                                                                       
+print(f'{Fore.BLUE} BY: MahmoudJOO{Style.RESET_ALL}') 
+print ("       ")
 
-a = input ("Enter The Pcap File Path:  " '\n')
-
-print ("$$$$$$$$$$$$$$$$$$   PLEASE WAIT UNTIL SCAN FINISH :)    $$$$$$$$$$$$$$$$$$$" '\n')  
-
-subprocess.run(f'tshark -T fields -e ip.src -r {a}>SM1.txt ', shell=True ,text=False)
+a = input ("Enter The Pcap File Path:  ")
+print ("       ")
+print(f'{Fore.RED}##################   PLEASE WAIT UNTIL SCAN FINISH  ##################{Style.RESET_ALL}')
+print ("       ")
+subprocess.run(f'tshark -T fields -e ip.src -r {a}>SM1.txt ', shell=True )
 subprocess.run('cat -n SM1.txt | sort -uk2 | sort -nk1 | cut -f2->HOSTip.txt', shell=True)
 
+subprocess.run('echo  "\033[1;91m----------------------------------\033[m" > RESULTS.txt ' , shell=True)
+subprocess.run('echo "\033[1;93mIdentified Protocol For Each HOST\033[m" >>RESULTS.txt' , shell=True)
+subprocess.run('echo "\033[1;91m-----------------------------------\033[m" >> RESULTS.txt ' , shell=True)
+subprocess.run('echo   >> RESULTS.txt' , shell=True)
 
-subprocess.run(f'tshark -T fields -e ip.src -r {a}>SM1.txt ', shell=True ,text=False)
-subprocess.run('cat -n SM1.txt | sort -uk2 | sort -nk1 | cut -f2->HOSTip.txt', shell=True)
-
-subprocess.run('echo ------------------------------------- > RESULTS.txt ' , shell=True)
-subprocess.run('echo Identified protocol for each HOST    >>RESULTS.txt' , shell=True)
-subprocess.run('echo ------------------------------------- >> RESULTS.txt ' , shell=True)
-
-subprocess.run('nmap  -T5 -sO -iL HOSTip.txt -oG - | grep -Ev "Nmap|Status" >> RESULTS.txt ' , shell=True )
+subprocess.run('nmap  -T5 -sO --open -iL HOSTip.txt -oG - | grep -Ev "Nmap|Status" >> RESULTS.txt ' , shell=True )
 
 subprocess.run('echo   >> RESULTS.txt' , shell=True)
 
-subprocess.run('echo =========================================== >> RESULTS.txt ' , shell=True)
+subprocess.run('echo  "\033[1;32m=======================================\033[m" >> RESULTS.txt ' , shell=True)
 
 subprocess.run('echo   >> RESULTS.txt' , shell=True)
 
 print ("SCAN Identified protocol DONE :    " '\n')
 
-subprocess.run('echo ---------------------------------- >> RESULTS.txt ' , shell=True)
-subprocess.run('echo OS and Open Ports for each HOST   >> RESULTS.txt' , shell=True)
-subprocess.run('echo ---------------------------------- >> RESULTS.txt ' , shell=True)
+subprocess.run('echo  "\033[1;91m---------------------------------\033[m " >> RESULTS.txt ' , shell=True)
+subprocess.run('echo "\033[1;93mOS And Open Ports For Each HOST \033[m"  >>RESULTS.txt' , shell=True)
+subprocess.run('echo "\033[1;91m---------------------------------\033[m" >> RESULTS.txt ' , shell=True)
+subprocess.run('echo   >> RESULTS.txt' , shell=True)
 
-subprocess.run('nmap -O -T5 --open -iL HOSTip.txt | grep -Ev "shown|Warning|detection|hops|done|Starting|Host|closed" >> RESULTS.txt ' , shell=True)
+subprocess.run('nmap -O -T4 --open -iL HOSTip.txt | grep -Ev "shown|Warning|detection|hops|done|Starting|Host|closed|Runing|CPE" | cut -c 1-50 >> RESULTS.txt ' , shell=True)    
+
 
 print ("SCAN OS & Open Ports DONE :        " '\n')
 
-
+print(f'{Fore.GREEN}       __..._   _...__{Style.RESET_ALL}'  )
+print(f'{Fore.GREEN}  _..-"      `Y`      "-._{Style.RESET_ALL}'  )
+print(f'{Fore.GREEN}  \           |           /{Style.RESET_ALL}'  )
+print(f'{Fore.GREEN}  \\     THE RESULTS      //{Style.RESET_ALL}'  )
+print(f'{Fore.GREEN}  \\\          |         ///{Style.RESET_ALL}')
+print(f'{Fore.GREEN}   \\\ _..---. |.---.._ ///{Style.RESET_ALL}')
+print(f'{Fore.GREEN}    \\`_..--- .Y.---.._`//{Style.RESET_ALL}')
+print ("         ")
 subprocess.run('cat RESULTS.txt' , shell=True)
