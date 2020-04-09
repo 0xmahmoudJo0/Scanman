7a8e5858cc88bb0e870ef69c600b7d5d8b0857b1
Mahmoud-joo <56273659+Mahmoud-joo@users.noreply.github.com>
2020-04-08 21:18:59 -0500

    Update Scanman.py

--- Scanman.py
+++ Scanman.py
@@ -20,13 +20,31 @@ print ("$$$$$$$$$$$$$$$$$$   PLEASE WAIT UNTIL SCAN FINISH :)    $$$$$$$$$$$$$$$
 subprocess.run(f'tshark -T fields -e ip.src -r {a}>SM1.txt ', shell=True ,text=False)
 subprocess.run('cat -n SM1.txt | sort -uk2 | sort -nk1 | cut -f2->HOSTip.txt', shell=True)
 
-subprocess.run('nmap  -T5 -sO -iL HOSTip.txt -oG - | grep -Ev "Nmap|Status" > RESULTS.txt ' , shell=True )
+
+subprocess.run(f'tshark -T fields -e ip.src -r {a}>SM1.txt ', shell=True ,text=False)
+subprocess.run('cat -n SM1.txt | sort -uk2 | sort -nk1 | cut -f2->HOSTip.txt', shell=True)
+
+subprocess.run('echo ------------------------------------- > RESULTS.txt ' , shell=True)
+subprocess.run('echo Identified protocol for each HOST    >>RESULTS.txt' , shell=True)
+subprocess.run('echo ------------------------------------- >> RESULTS.txt ' , shell=True)
+
+subprocess.run('nmap  -T5 -sO -iL HOSTip.txt -oG - | grep -Ev "Nmap|Status" >> RESULTS.txt ' , shell=True )
+
+subprocess.run('echo   >> RESULTS.txt' , shell=True)
+
+subprocess.run('echo =========================================== >> RESULTS.txt ' , shell=True)
+
+subprocess.run('echo   >> RESULTS.txt' , shell=True)
+
 print ("SCAN Identified protocol DONE :    " '\n')
 
-subprocess.run('echo -------------------------------------------------------------------------------------------------------- >> RESULTS.txt ' , shell=True)
+subprocess.run('echo ---------------------------------- >> RESULTS.txt ' , shell=True)
+subprocess.run('echo OS and Open Ports for each HOST   >> RESULTS.txt' , shell=True)
+subprocess.run('echo ---------------------------------- >> RESULTS.txt ' , shell=True)
 
 subprocess.run('nmap -O -T5 --open -iL HOSTip.txt | grep -Ev "shown|Warning|detection|hops|done|Starting|Host|closed" >> RESULTS.txt ' , shell=True)
+
 print ("SCAN OS & Open Ports DONE :        " '\n')
 
-print ("the ip hosts in HOSTip.txt   ")
-print ("the output is in RESULTS.txt ")
+
+subprocess.run('cat RESULTS.txt' , shell=True)
