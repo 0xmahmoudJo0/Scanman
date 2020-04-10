3596ad2c8c03d28ee1c249cc7cdb19cb49cecda1
Mahmoud-joo <56273659+Mahmoud-joo@users.noreply.github.com>
2020-04-09 19:03:49 -0500

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,5 +1,6 @@
-sunprocess==pip3 install subprocess 
+sunprocess==pip install subprocess 
 updating==apt update
 python3==apt install python3
 tshark==apt install tshark
 nmap==apt install nmap
+colorama==pip install colorama
